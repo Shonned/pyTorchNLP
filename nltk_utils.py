c8633eb1695df492bb584146e8b007edcc5757c1
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-18 13:47:38 +0100

    bag of words

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 import torch
 import nltk
+import numpy as np
 nltk.download('punkt')
 
 from nltk.stem.porter import PorterStemmer
@@ -13,4 +14,17 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, word in enumerate(all_words):
+        if word in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
